e729c837157a3f8cfeefe941604c6d1361649fe5
Rafou2898 <rafael.dousse@hes-so.ch>
2025-12-12 10:48:18 +0100

    classes declaration

---
+++ threaded_lockfree/work_stealing_deque.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <atomic>
+#include <vector>
+#include <stdexcept>
+
+template <typename T>
+class WorkStealingDeque
+{
+private:
+
+public:
+    WorkStealingDeque()
+    {
+
+    }
+
+private:
+};
---
+++ threaded_lockfree/work_stealing_runner.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "task.hpp"
+#include "work_stealing_deque.hpp"
+#include <thread>
+#include <vector>
+#include <atomic>
+#include <random>
+
+class WorkStealingRunner : public TaskRunner
+{
+private:
+
+
+public:
+    WorkStealingRunner() : _
+    {
+
+    }
+    ~WorkStealingRunner()
+    {
+
+    }
+};
